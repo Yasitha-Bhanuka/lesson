756e3bd58b4b41aad5b972df388d45c2629f74a2
Yasitha-Bhanuka <yasithabhanukac@gmail.com>
2024-01-07 14:57:01 +0530

    querySelectorAll for delete buttons

--- todo/11x.js
+++ todo/11x.js
@@ -3,86 +3,95 @@ const myarray = JSON.parse(localStorage.getItem('myarray')) || [{
   dueDate
   }];  
     
+renderDetails();
+
+document.querySelector('.delete-btn').addEventListener('click', function(){
+  myarray.splice(0, 1);
+  saveToStorage();
+  renderDetails();
+});
+
+document.querySelectorAll('.delete-btn').forEach(function(btn, index){
+  btn.addEventListener('click', function(){
+    myarray.splice(index, 1);
+    saveToStorage();
     renderDetails();
+  });
+});
 
-    document.querySelector('.delete-btn').addEventListener('click', function(){
-      myarray.splice(0, 1);
-      saveToStorage();
-      renderDetails();
+
+function btnFunction(event){
+  
+  if (event.key === 'Enter'){
+    addToArray();
+  }
+  
+}
+
+function addToArray(){
+  
+  const inputElement = document.querySelector('.js-input');
+  
+  const dateInputElement = document.querySelector('.js-date-input');
+  
+  /*const namevalue = inputElement.value;
+  const datevalue = dateInputElement.value; // in this method without using shorthand property
+  myarray.push({
+    name: namevalue,
+    dueDate: datevalue
+  });*/
+
+  // using shortHand property
+
+  const dueDate = dateInputElement.value;
+  const name = inputElement.value;
+  if (name && dueDate){
+    myarray.push({
+      name,
+      dueDate
     });
+
+  } else{
+
+    alert('Please enter the value');
+
+  }
+  
+  renderDetails();
+
+  saveToStorage();
+  
+  inputElement.value = '';
+
+  dateInputElement.value = '';
+  
+}
+
+function renderDetails(){
+  
+  let ListElement = '';
     
-    function btnFunction(event){
-      
-      if (event.key === 'Enter'){
-        addToArray();
-      }
-      
-    }
+  const displayElement = document.querySelector('.js-list');
+
+  myarray.forEach(function(todoObject, index){
+
+    const { name, dueDate } = todoObject;
     
-    function addToArray(){
-      
-      const inputElement = document.querySelector('.js-input');
-      
-      const dateInputElement = document.querySelector('.js-date-input');
-      
-      /*const namevalue = inputElement.value;
-      const datevalue = dateInputElement.value; // in this method without using shorthand property
-      myarray.push({
-        name: namevalue,
-        dueDate: datevalue
-      });*/
+    const todoraw = `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button class="delete-btn"
 
-      // using shortHand property
-
-      const dueDate = dateInputElement.value;
-      const name = inputElement.value;
-      if (name && dueDate){
-        myarray.push({
-          name,
-          dueDate
-        });
-
-      } else{
-
-        alert('Please enter the value');
-
-      }
-      
-      renderDetails();
-
-      saveToStorage();
-      
-      inputElement.value = '';
-
-      dateInputElement.value = '';
-      
-    }
-    
-    function renderDetails(){
-      
-      let ListElement = '';
-        
-      const displayElement = document.querySelector('.js-list');
-
-      myarray.forEach(function(todoObject, index){
-
-        const { name, dueDate } = todoObject;
-        
-        const todoraw = `
-          <div>${name}</div>
-          <div>${dueDate}</div>
-          <button class="delete-btn"
-    
 >Delete</button>
-          `;
-          
-        ListElement += todoraw;
+      `;
+      
+    ListElement += todoraw;
 
-      });
+  });
 
-      displayElement.innerHTML = ListElement;
-   }
+  displayElement.innerHTML = ListElement;
+}
 
-   function saveToStorage(){
-    localStorage.setItem('myarray', JSON.stringify(myarray));
-   }
+function saveToStorage(){
+localStorage.setItem('myarray', JSON.stringify(myarray));
+}
